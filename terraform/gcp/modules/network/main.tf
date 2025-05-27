832e548eba5d19d75f48a5f1ec9067b77c1aef85
younghak9905 <yeonghak9905@gmail.com>
2025-05-27 20:09:41 +0900

    feat: nat, 고정 IP 추가

--- terraform/gcp/modules/network/main.tf
+++ terraform/gcp/modules/network/main.tf
@@ -19,3 +19,36 @@ resource "google_compute_subnetwork" "subnets" {
   private_ip_google_access = each.value.private_ip_google_access
 
 }
+
+# Cloud Router
+resource "google_compute_router" "router" {
+  name    = "${var.vpc_name}-router"
+  region  = var.region
+  network = google_compute_network.vpc.name
+}
+
+# 고정 외부 IP (Cloud NAT용)
+resource "google_compute_address" "nat_ip" {
+  name   = "${var.vpc_name}-nat-ip"
+  region = var.region
+}
+
+# Cloud NAT
+resource "google_compute_router_nat" "nat" {
+  name                               = "${var.vpc_name}-nat"
+  router                             = google_compute_router.router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "MANUAL_ONLY"
+  nat_ips                            = [google_compute_address.nat_ip.self_link]
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+
+  dynamic "subnetwork" {
+    for_each = {
+      for subnet in var.nat_subnets : subnet.name => subnet
+    }
+    content {
+      name                    = google_compute_subnetwork.subnets[subnetwork.key].self_link
+      source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+    }
+  }
+}
