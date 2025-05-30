b08bf8b3e2aa951543abf91b8987912ea7c81f84
younghak9905 <yeonghak9905@gmail.com>
2025-05-30 16:36:19 +0900

    hotfix: nat_subnet_info가 잘 나오는지 확인

--- terraform/gcp/environments/shared/output.tf
+++ terraform/gcp/environments/shared/output.tf
@@ -21,11 +21,12 @@ output "nat_b_subnet_self_link" {
 
 output "nat_subnet_info" {
   value = {
-    for k, s in google_compute_subnetwork.shared_subnets :
-    k => {
+    for s in google_compute_subnetwork.shared_subnets :
+    s.name => {
       name      = s.name
       self_link = s.self_link
       cidr      = s.ip_cidr_range
-    } if contains(["nat-a", "nat-b"], k)
+    }
+    if can(regex("-nat-", s.name))
   }
 }
