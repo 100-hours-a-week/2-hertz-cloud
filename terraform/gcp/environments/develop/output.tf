c64a0940472df0b96fcb547c826f778138f06fde
younghak9905 <yeonghak9905@kyonggi.ac.kr>
2025-05-31 23:53:07 +0900

    hotfix: bugfix

--- terraform/gcp/environments/develop/output.tf
+++ terraform/gcp/environments/develop/output.tf
@@ -1,9 +1,3 @@
-
-
-output "backend_internal_lb_ip" {
-  description = "백엔드 Internal HTTP LB(8080) 사설 IP"
-  value       = module.backend_internal_lb.internal_lb_ip
-}
 output "frontend_lb_ip" {
   description = "프론트엔드 외부 HTTPS(443) LB IP"
   value       = module.frontend_lb.forwarding_rule_ip
